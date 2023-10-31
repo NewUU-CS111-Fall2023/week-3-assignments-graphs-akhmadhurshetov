ff9e35d034da7e1e56d919424b1a116435ff3c07
akhmadhurshetov <122173327+akhmadhurshetov@users.noreply.github.com>
2023-10-31 23:43:23 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -8,6 +8,7 @@
 #include "task_1.h"
 #include "task_2.h"
 #include "task_3.h"
+#include "task_4.h"
 
 int main() {
     std::cout << "Task 1" << std::endl;
@@ -17,7 +18,7 @@ int main() {
     std::cout << "Task 3" << std::endl;
     Transformation();
     std::cout << "Task 4" << std::endl;
-    // call for task 4
+    Genome();
     std::cout << "Task 5" << std::endl;
     // call for task 5
     return 0;
